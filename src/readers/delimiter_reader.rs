b545a34ee7d6ebdb77147174abaaa814306da0ed
Harry <hello@majored.pw>
2022-02-11 15:00:17 +0000

    Alter AsyncDelimiterReader to use AsyncPrependReader

--- src/readers/delimiter_reader.rs
+++ src/readers/delimiter_reader.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2021 Harry [Majored] [hello@majored.pw]
 // MIT License (https://github.com/Majored/rs-async-io-utilities/blob/main/LICENSE)
 
+use crate::readers::prepend_reader::AsyncPrependReader;
 use tokio::io::{AsyncRead, ReadBuf};
 
 use std::pin::Pin;
@@ -11,9 +12,8 @@ use std::task::{Context, Poll};
 /// Whilst the matched state of this wrapper is true, [`Self::poll_read()`] will return `Poll::Ready(Ok(()))`,
 /// signifying EOF.
 pub struct AsyncDelimiterReader<R: AsyncRead + Unpin> {
-    inner: R,
+    inner: AsyncPrependReader<R>,
     delimiter: Vec<u8>,
-    buffer: Vec<u8>,
     matched: bool,
 }
 
@@ -30,8 +30,7 @@ impl<R: AsyncRead + Unpin> AsyncDelimiterReader <R> {
         }
 
         Self {
-            inner,
-            buffer: Vec::new(),
+            inner: AsyncPrependReader::new(inner),
             delimiter,
             matched: false,
         }
@@ -49,7 +48,7 @@ impl<R: AsyncRead + Unpin> AsyncDelimiterReader <R> {
 
     /// Consumes this wrapper and returns the inner [`AsyncRead`] reader.
     pub fn into_inner(self) -> R {
-        self.inner
+        self.inner.into_inner()
     }
 }
 
@@ -69,7 +68,7 @@ impl<R: AsyncRead + Unpin> AsyncRead for AsyncDelimiterReader <R> {
 
         if let Some((full_match, match_index)) = match_delimiter(&mut self, read_slice) {
             let actual_read_slice = &read_slice[match_index + self.delimiter.len()..];
-            self.buffer.extend_from_slice(actual_read_slice);
+            self.inner.prepend(actual_read_slice);
 
             b.set_filled(match_index);
 
@@ -87,12 +86,6 @@ fn fill_buffer<W: AsyncRead + Unpin>(
     reader: &mut Pin<&mut AsyncDelimiterReader<W>>, c: &mut Context<'_>, b: &mut ReadBuf<'_>
 ) -> (usize, usize, Poll<tokio::io::Result<()>>) {
     let prev_len = b.filled().len();
-
-    // Append the local buffer first if it's not empty.
-    if !reader.buffer.is_empty() {
-        b.put_slice(&reader.buffer);
-        reader.buffer.clear();
-    }
 
     let poll = Pin::new(&mut reader.inner).poll_read(c, b);
     let new_len = b.filled().len();
