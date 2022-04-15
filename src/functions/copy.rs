691f1c3887bad1104b7e77a1ffa15e13835f1f6b
Harry <hello@majored.pw>
2022-04-15 11:22:31 +0100

    Reduce suggested buffer size for the moment due to stack overflow on Windows in Debug

--- src/functions/copy.rs
+++ src/functions/copy.rs
@@ -4,8 +4,10 @@
 use std::io::Result;
 use tokio::io::{AsyncRead, AsyncWrite, BufReader};
 
-/// A suggested buffer size for IO operations; equal to 64Kb.
-pub const SUGGESTED_BUFFER_SIZE: usize = 65536;
+/// A suggested buffer size for IO operations; equal to 16Kb.
+/// 
+/// Note: This was originally 64Kb, however, it was causing a stack overflow for rs-async-zip on Windows in Debug mode.
+pub const SUGGESTED_BUFFER_SIZE: usize = 16384;
 
 /// A buffered alternative to [`tokio::io::copy`] for high-throughput use cases.
 pub async fn copy<R, W>(reader: &mut R, writer: &mut W, buf_size: usize) -> Result<()> 
